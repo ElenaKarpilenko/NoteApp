36fcb326a4cbb16c0d0244d20c94db1b4e292c0a
elenakarpilenko <elenakarpilenko787@gmail.com>
2024-09-16 16:43:04 +0600

    hw5

--- app/src/main/java/com/example/noteapplication/ui/fragment/notes/NoteFragment.kt
+++ app/src/main/java/com/example/noteapplication/ui/fragment/notes/NoteFragment.kt
@@ -6,21 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.noteapplication.App
 import com.example.noteapplication.R
 import com.example.noteapplication.databinding.FragmentNoteBinding
 import com.example.noteapplication.ui.adapter.NoteAdapter
+import com.example.noteapplication.utils.SharedPreferenceHelper
 
 class NoteFragment : Fragment() {
 
     private lateinit var binding: FragmentNoteBinding
     private lateinit var noteAdapter: NoteAdapter
-//    private var list: ArrayList<NoteModel> = ArrayList()
+    private var isGridLayout = false
+    private lateinit var sharedPreferenceHelper: SharedPreferenceHelper
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         binding = FragmentNoteBinding.inflate(inflater, container, false)
         return binding.root
@@ -28,6 +31,8 @@ class NoteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        sharedPreferenceHelper = SharedPreferenceHelper(requireContext())
+        isGridLayout = sharedPreferenceHelper.getIsGridLayout()
         noteAdapter = NoteAdapter()
         initialize()
         setUpListeners()
@@ -37,8 +42,22 @@ class NoteFragment : Fragment() {
 
     private fun initialize() {
         binding.rvNote.apply {
-            layoutManager = LinearLayoutManager(requireContext())
+            layoutManager = if (isGridLayout) {
+                GridLayoutManager(requireContext(), 2)
+            } else {
+                LinearLayoutManager(requireContext())
+            }
             adapter = noteAdapter
+        }
+    }
+
+    private fun updateLayoutButtonIcon() = with(binding) {
+        if (isGridLayout) {
+            gridLayout.visibility = View.GONE
+            linearLayout.visibility = View.VISIBLE
+        } else {
+            gridLayout.visibility = View.VISIBLE
+            linearLayout.visibility = View.GONE
         }
     }
 
@@ -46,10 +65,22 @@ class NoteFragment : Fragment() {
         addBtn.setOnClickListener {
             findNavController().navigate(R.id.action_noteFragment_to_detailNoteFragment)
         }
+        gridLayout.setOnClickListener {
+            isGridLayout = true
+            binding.rvNote.layoutManager = GridLayoutManager(requireContext(), 2)
+            sharedPreferenceHelper.setIsGridLayout(isGridLayout)
+            updateLayoutButtonIcon()
+        }
+        linearLayout.setOnClickListener {
+            isGridLayout = false
+            binding.rvNote.layoutManager = LinearLayoutManager(requireContext())
+            sharedPreferenceHelper.setIsGridLayout(isGridLayout)
+            updateLayoutButtonIcon()
+        }
     }
 
     private fun getData() {
-        App().getInstance()?.noteDao()?.getAll()?.observe(viewLifecycleOwner){
+        App().getInstance()?.noteDao()?.getAll()?.observe(viewLifecycleOwner) {
             noteAdapter.submitList(it)
         }
 
